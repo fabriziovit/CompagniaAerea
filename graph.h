9da4a3159d90c5fd988db834f1a20717080051b3
Fabrizio Vitale <fabrizio.vitale1@outlook.com>
2020-05-26 17:35:01 +0200

    Aggiunta grafo da definire con i suoi metodi e registrazione

--- graph.h
+++ graph.h
@@ -1,4 +1,20 @@
 //
 // Created by Fabrizio on 25/05/2020.
 //
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
+typedef struct edge {
+    char aereoporto[4];
+    int km;
+    struct node *next;
+}Edge;
+
+typedef struct ArrayNode{
+    int n;
+    Edge **adj;
+}Graph;
+
+int creaNodo(Graph *P, char *aereoporto,int km, int insert);
+void Aggiungi(Graph *P, char matricola[], int insert);
