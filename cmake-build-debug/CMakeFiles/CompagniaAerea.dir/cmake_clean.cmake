1ae082e12c3f79ae57a7c31fd091e9f8f0ca91e9
fabriziovit <59337335+fabriziovit@users.noreply.github.com>
2020-05-27 11:51:47 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/CompagniaAerea.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CompagniaAerea.dir/graph.c.obj"
+  "CMakeFiles/CompagniaAerea.dir/main.c.obj"
+  "CMakeFiles/CompagniaAerea.dir/sqlite3.c.obj"
+  "CompagniaAerea.exe"
+  "CompagniaAerea.exe.manifest"
+  "CompagniaAerea.pdb"
+  "libCompagniaAerea.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CompagniaAerea.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
