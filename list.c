e055224b16ee2c3cf8ba3ef2fc8da2dc03d68137
GianlucaPignieri <59359832+GianlucaPignieri@users.noreply.github.com>
2020-05-29 11:21:11 +0200

    Collegamento della lista al DB e modifica di alcune funzioni.

--- list.c
+++ list.c
@@ -8,6 +8,7 @@
 #include <string.h>
 #include "list.h"
 #include "sqlite3.h"
+sqlite3 *db;
 
 Prenotazioni InizializzaL(char partenza[100], char destinazione[100], char utente[100]){
     Prenotazioni L = (Prenotazioni)malloc(sizeof(struct Nodo));
@@ -28,6 +29,26 @@ void StampaL(Prenotazioni L){
     }
 }
 
+void InserisciLDB(char partenza[100], char destinazione[100], char utente[100]){
+    char *sql;
+    int rc;
+    sqlite3_stmt *stmt;
+
+    sql = "INSERT INTO PRENOTATO(USERNAME, AEROPART, AERODEST) VALUES(?1, ?2, ?3)";
+    sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+    sqlite3_bind_text(stmt, 1, utente, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 2, partenza, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 3, destinazione, -1, SQLITE_STATIC);
+    rc = sqlite3_step(stmt);
+    if(rc != SQLITE_DONE){
+        printf("Errore durante l'inserimento \n");
+        sqlite3_close(db);
+        exit(-1);
+    }
+    sqlite3_finalize(stmt);
+    printf("\nPrenotazione confermata \n");
+}
+
 void InserisciL(Prenotazioni *L, char partenza[100], char destinazione[100], char utente[100]){
     Prenotazioni ultimo;
     Prenotazioni temp;
@@ -40,23 +61,71 @@ void InserisciL(Prenotazioni *L, char partenza[100], char destinazione[100], cha
             ultimo = ultimo->next;
         ultimo->next = temp;
     }
+    InserisciLDB(partenza, destinazione, utente);
 }
 
-void EliminaElem(Prenotazioni L, char partenza[100], char destinazione[100], char utente[100]){
-    struct Nodo *tmp = L->next;
+//DA FIXARE QUANDO BISOGNA ELIMINARE IL PRIMO ELEMENTO
+void EliminaLDB(char partenza[100], char destinazione[100], char utente[100]){
+    char *sql;
+    int rc;
+    sqlite3_stmt *stmt;
+
+    sql = "DELETE FROM PRENOTATO WHERE USERNAME = ?1 AND AEROPART = ?2 AND AERODEST = ?3;";
+    sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+    sqlite3_bind_text(stmt, 1, utente, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 2, partenza, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 3, destinazione, -1, SQLITE_STATIC);
+    rc = sqlite3_step(stmt);
+    if(rc != SQLITE_DONE){
+        printf("Exit \n");
+        sqlite3_close(db);
+        exit(-1);
+    }
+    sqlite3_finalize(stmt);
+    printf("\nLa prenotazione dell' utente %s del volo che parte da %s con destinazione a %s e' stata correttamente cancellata. \n", utente, partenza, destinazione);
+}
+
+
+//DA FIXARE QUANDO NON VENGONO PASSATI PARAMETRI SBAGLIATI
+void EliminaElem(Prenotazioni *L, char partenza[100], char destinazione[100], char utente[100]){
+    struct Nodo *tmp = *L;
     struct Nodo *prev;
 
-    if(tmp != NULL && (strcmp(tmp->partenza, partenza) == 0)){
-        L->next = tmp->next;
+    if(tmp != NULL && ((strcmp(tmp->partenza, partenza) == 0) && (strcmp(tmp->destinazione, destinazione) == 0) && (strcmp(tmp->utente, utente) == 0))){
+        *L = tmp->next;
         free(tmp);
-        printf("ELIMINATO");
+        return;
     }
-    while(tmp != NULL && (strcmp(tmp->partenza, partenza) != 0)){
+    while(tmp != NULL && ((strcmp(tmp->partenza, partenza) != 0) && (strcmp(tmp->destinazione, destinazione) != 0) && (strcmp(tmp->utente, utente) != 0))){
         prev = tmp;
         tmp = tmp->next;
     }
     if(tmp == NULL)
-        printf("ERROR");
+        return;
     prev->next = tmp->next;
     free(tmp);
+    EliminaLDB(partenza, destinazione, utente);
 }
+
+//Codice per provare le funzioni
+/*char mat[] = "pep";
+    char pos[] = "wes";
+    char fas[] = "sgrodolo";
+    char lol[] = "res";
+    char holt[] = "holt";
+    char mm[] = "djs";
+    char ss[] = "sw";
+    char bb[] = "dsa";
+    char gg[] = "qqq";
+    char sa[] = "SOP";
+    Prenotazioni *L= NULL;
+    Graph *G;
+
+    creaDatabase(G);
+    G = CreaGrafo(1);
+
+    InserisciL(&L, mat, pos, fas);
+    InserisciL(&L, lol, holt, mm);
+    InserisciL(&L, ss, bb, gg);
+    EliminaElem(&L, sa, holt, mm);
+    StampaL(L);*/
--- list.h
+++ list.h
@@ -19,4 +19,6 @@ typedef struct Nodo* Prenotazioni;
 Prenotazioni InizializzaL(char partenza[100], char destinazione[100], char utente[100]);
 void StampaL(Prenotazioni L);
 void InserisciL(Prenotazioni *L, char partenza[100],  char destinazione[100], char utente[100]);
-void EliminaElem(Prenotazioni L, char partenza[100], char destinazione[100], char utente[100]);
+void EliminaElem(struct Nodo **L, char partenza[100], char destinazione[100], char utente[100]);
+void InserisciLDB(char partenza[100], char destinazione[100], char utente[100]);
+void EliminaLDB(char partenza[100], char destinazione[100], char utente[100]);
